874ab5ee391ef54588e5236190aef46a78256c2c
viji <vijic.babu1995@gmail.com>
2025-03-21 16:01:51 +0400

    change port

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ app.use("/dashboard", require("./routes/dashboard"));
 app.get("/", (req, res) => res.send("CRM Backend Server Running"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port ${PORT}"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
